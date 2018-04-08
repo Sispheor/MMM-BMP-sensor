2ce97a108e3258208b2fe19074488f4384a6d7d1
nico <nico.marcq@gmail.com>
2018-04-08 21:42:20 +0200

    add nedb storage

--- MMM-BMP-sensor.js
+++ MMM-BMP-sensor.js
@@ -3,13 +3,26 @@
 Module.register("MMM-BMP-sensor", {
 
     defaults: {
+        limitKeepBmpData: "1h"
 
     },
+
+    // empty database by default
+    currentDatabase: [],
 
     start: function() {
         console.log("Starting module: " + this.name);
         this.loaded = false;
+
+        // give the config to the node helper
+        let configPayload = {
+            limitKeepBmpData: this.getUpdateIntervalMillisecondFromString(this.config.limitKeepBmpData)
+        };
+
+        this.sendSocketNotification('INIT_HELPER', configPayload);
         this.getNewBmpData();
+        this.sendSocketNotification('GET_CURRENT_DATABASE', null);
+
     },
 
     getScripts: function() {
@@ -28,75 +41,75 @@ Module.register("MMM-BMP-sensor", {
             return wrapper;
         }
 
-        // Start building table.
-        let dataTable = document.createElement("table");
-        dataTable.className = "small";
-
-        // add row
-        let temperatureRow = document.createElement("tr");
-        let altitudeRow = document.createElement("tr");
-        let seaLevelPressureRow = document.createElement("tr");
-        let pressureRow = document.createElement("tr");
-
-        // add temperature row
-        let temperatureCell1 = document.createElement("td");
-        temperatureCell1.innerHTML = "Temperature";
-        temperatureRow.appendChild(temperatureCell1);
-        let temperatureCell2 = document.createElement("td");
-        temperatureCell2.innerHTML = this.result.temperature + " °C";
-        temperatureRow.appendChild(temperatureCell2);
-        dataTable.appendChild(temperatureRow);
-
-        // add altitude row
-        let altitudeCell1 = document.createElement("td");
-        altitudeCell1.innerHTML = "Altitude";
-        altitudeRow.appendChild(altitudeCell1);
-        let altitudeCell2 = document.createElement("td");
-        altitudeCell2.innerHTML = this.result.altitude + " m";
-        altitudeRow.appendChild(altitudeCell2);
-        dataTable.appendChild(altitudeRow);
-
-        // add seaLevelPressure row
-        let seaLevelPressureCell1 = document.createElement("td");
-        seaLevelPressureCell1.innerHTML = "Sea level pressure";
-        seaLevelPressureRow.appendChild(seaLevelPressureCell1);
-        let seaLevelPressureCell2 = document.createElement("td");
-        seaLevelPressureCell2.innerHTML = Math.round(this.result.sealevel_pressure/100) + " hPa";
-        seaLevelPressureRow.appendChild(seaLevelPressureCell2);
-        dataTable.appendChild(seaLevelPressureRow);
-
-        // add pressure row
-        let pressureCell1 = document.createElement("td");
-        pressureCell1.innerHTML = "Pressure";
-        pressureRow.appendChild(pressureCell1);
-        let pressureCell2 = document.createElement("td");
-        pressureCell2.innerHTML = Math.round( this.result.pressure/100 ) + " hPa";
-        pressureRow.appendChild(pressureCell2);
-        dataTable.appendChild(pressureRow);
-
-        // add the table to the view
-        // wrapper.appendChild(dataTable);
-
-        // add a gauge
-        let gauge = new LinearGauge({
-            renderTo: document.createElement('canvas'),
-            width: 160,
-            height: 600,
-            borderRadius: 20,
-            borders: 0,
-            barStrokeWidth: 20,
-            minValue: 940,
-            maxValue: 1080,
-            minorTicks: 10,
-            majorTicks: [960,970,980,990,1000,1010,1020,1030,1040,1050,1060],
-            value: this.result.pressure/100,
-            units: "hPa",
-            colorPlate: "black",
-            colorNeedle: "white",
-            tickSide: "left",
-            numberSide: "left",
-            needleSide: "left",
-        });
+        // // Start building table.
+        // let dataTable = document.createElement("table");
+        // dataTable.className = "small";
+        //
+        // // add row
+        // let temperatureRow = document.createElement("tr");
+        // let altitudeRow = document.createElement("tr");
+        // let seaLevelPressureRow = document.createElement("tr");
+        // let pressureRow = document.createElement("tr");
+        //
+        // // add temperature row
+        // let temperatureCell1 = document.createElement("td");
+        // temperatureCell1.innerHTML = "Temperature";
+        // temperatureRow.appendChild(temperatureCell1);
+        // let temperatureCell2 = document.createElement("td");
+        // temperatureCell2.innerHTML = this.result.temperature + " °C";
+        // temperatureRow.appendChild(temperatureCell2);
+        // dataTable.appendChild(temperatureRow);
+        //
+        // // add altitude row
+        // let altitudeCell1 = document.createElement("td");
+        // altitudeCell1.innerHTML = "Altitude";
+        // altitudeRow.appendChild(altitudeCell1);
+        // let altitudeCell2 = document.createElement("td");
+        // altitudeCell2.innerHTML = this.result.altitude + " m";
+        // altitudeRow.appendChild(altitudeCell2);
+        // dataTable.appendChild(altitudeRow);
+        //
+        // // add seaLevelPressure row
+        // let seaLevelPressureCell1 = document.createElement("td");
+        // seaLevelPressureCell1.innerHTML = "Sea level pressure";
+        // seaLevelPressureRow.appendChild(seaLevelPressureCell1);
+        // let seaLevelPressureCell2 = document.createElement("td");
+        // seaLevelPressureCell2.innerHTML = Math.round(this.result.sealevel_pressure/100) + " hPa";
+        // seaLevelPressureRow.appendChild(seaLevelPressureCell2);
+        // dataTable.appendChild(seaLevelPressureRow);
+        //
+        // // add pressure row
+        // let pressureCell1 = document.createElement("td");
+        // pressureCell1.innerHTML = "Pressure";
+        // pressureRow.appendChild(pressureCell1);
+        // let pressureCell2 = document.createElement("td");
+        // pressureCell2.innerHTML = Math.round( this.result.pressure/100 ) + " hPa";
+        // pressureRow.appendChild(pressureCell2);
+        // dataTable.appendChild(pressureRow);
+        //
+        // // add the table to the view
+        // // wrapper.appendChild(dataTable);
+        //
+        // // add a gauge
+        // let gauge = new LinearGauge({
+        //     renderTo: document.createElement('canvas'),
+        //     width: 160,
+        //     height: 600,
+        //     borderRadius: 20,
+        //     borders: 0,
+        //     barStrokeWidth: 20,
+        //     minValue: 940,
+        //     maxValue: 1080,
+        //     minorTicks: 10,
+        //     majorTicks: [960,970,980,990,1000,1010,1020,1030,1040,1050,1060],
+        //     value: this.result.pressure/100,
+        //     units: "hPa",
+        //     colorPlate: "black",
+        //     colorNeedle: "white",
+        //     tickSide: "left",
+        //     numberSide: "left",
+        //     needleSide: "left",
+        // });
         // wrapper.appendChild(gauge.options.renderTo);
         // gauge.draw();
 
@@ -110,15 +123,18 @@ Module.register("MMM-BMP-sensor", {
         let color = "rgba(255, 255, 255, 0.8)";
         let color2 = "rgba(255, 255, 255, 0.2)";
 
+        let labels = [];
+        let bmpData = [];
+        for (let i = 0; i < this.currentDatabase.length; i++) {
+            labels.push(this.currentDatabase[i].date);
+            bmpData.push(Math.round(this.currentDatabase[i].bmpData.pressure / 100));
+        }
+
 
         let data=  {
-            labels: [
-                new Date(2017,8,16,13,30),
-                new Date(2017,8,16, 13,35),
-                new Date(2017,8,16, 13, 40)
-            ],
+            labels: labels,
             datasets: [{
-                data: [980, 1000, 970],
+                data: bmpData,
                 borderColor: color,
             }]
         };
@@ -134,10 +150,10 @@ Module.register("MMM-BMP-sensor", {
                     type: 'time',
                     display: true,
                     time: {
-                        unit: 'minute',
+                        unit: 'minutes',
                         distribution: 'series',
                         displayFormats: {
-                             minute: 'HH:mm'
+                            minute: 'HH:mm'
                         }
                     },
                     gridLines: {
@@ -154,26 +170,80 @@ Module.register("MMM-BMP-sensor", {
             }
         };
 
-        var myChart = new Chart(ctx, {
+        new Chart(ctx, {
             type: 'line',
             data: data,
             options: options
         });
 
-// return the main div
+        // return the main div
         return wrapper;
     },
 
     getNewBmpData: function () {
-        this.sendSocketNotification('GET_BMP_DATA');
+        this.sendSocketNotification('GET_NEW_BMP_DATA');
     },
 
     socketNotificationReceived: function (notification, payload) {
-        if (notification === "BMP_DATA_RESULT") {
-            this.result = payload;
+        if (notification === "DATABASE_RESULT") {
+            this.currentDatabase = payload;
             this.loaded = true;
             this.updateDom();
         }
+    },
+
+    /** return a number of millisecond following the received intervalString
+     *  Interval string is composed by a number followed by a letter.
+     *  E.g:
+     *      intervalString 1m will return 60000
+     *      intervalString 10m will return 600000
+     * @param {string} intervalString - The interval to convert into second of the book.
+     */
+    getUpdateIntervalMillisecondFromString: function(intervalString) {
+        // console.log("[MMM-quote-of-the-day] testing string: "+ intervalString)
+        // the string must contains a number followed by a letter s or m or h or d. E.g: 50m
+        let regexString = new RegExp("^\\d+[smhd]{1}$");
+        let updateIntervalMillisecond = 0;
+
+        if (regexString.test(intervalString)){
+            console.log("[MMM-quote-of-the-day] valid updateInterval");
+            // split the integer from the letter
+            let regexInteger = "^\\d+";
+            let integer = intervalString.match(regexInteger);
+            // console.log("[MMM-quote-of-the-day] integer: " + integer);
+
+            // now get the letter
+            let regexLetter = "[smhd]{1}$";
+            let letter = intervalString.match(regexLetter);
+            // console.log("[MMM-quote-of-the-day] letter: '" + letter + "'");
+
+            // convert the letter into second
+            let millisecondsMultiplier = 1000;
+            switch (String(letter)) {
+                case "s":
+                    millisecondsMultiplier = 1000;
+                    break;
+                case "m":
+                    millisecondsMultiplier = 1000 * 60;
+                    break;
+                case "h":
+                    millisecondsMultiplier = 1000 * 60 * 60;
+                    break;
+                case "d":
+                    millisecondsMultiplier = 1000 * 60 * 60 * 24;
+                    break;
+            }
+
+            // convert the string into seconds
+            updateIntervalMillisecond = millisecondsMultiplier * integer
+
+        }else{
+            console.log("[MMM-quote-of-the-day] invalid updateInterval, set default to 1 day");
+            // set default interval to 1 day
+            updateIntervalMillisecond = 1000 * 60 * 60 * 24
+        }
+
+        return updateIntervalMillisecond
     },
 
 });
--- node_helper.js
+++ node_helper.js
@@ -1,19 +1,31 @@
 const NodeHelper = require('node_helper');
 const PythonShell = require('python-shell');
-
+const Datastore = require('nedb')
 
 module.exports = NodeHelper.create({
     start: function () {
         console.log(this.name + ' helper started');
 
+        this.helperConfig = {};
+
+        // create a database
+        this.db = new Datastore({
+            filename: '/tmp/mmm-bpm-datastore',
+            autoload: true
+        });
 
     },
 
     socketNotificationReceived: function (notification, payload) {
         console.log(this.name + " received a socket notification: " + notification + " - Payload: " + payload);
-
-        if (notification === 'GET_BMP_DATA') {
-            this.getBmpData();
+        if (notification === 'INIT_HELPER') {
+            this.helperConfig = payload
+        }
+        if (notification === 'GET_NEW_BMP_DATA') {
+            this.saveNewBmpEntryInDatabase();
+        }
+        if (notification === 'GET_CURRENT_DATABASE') {
+            this.getCurrentDatabase();
         }
 
     },
@@ -26,8 +38,14 @@ module.exports = NodeHelper.create({
         };
 
         // testing returns
-        let returnedData =  {"sealevel_pressure":"98029.00","pressure":"98018.00","altitude":"277.84","temperature":"22.90"}
-        self.sendSocketNotification('BMP_DATA_RESULT', returnedData);
+        return {
+            "sealevel_pressure": "98029.00",
+            "pressure": "98018.00",
+            "altitude": "277.84",
+            "temperature": "22.90"
+        };
+        // self.sendSocketNotification('BMP_DATA_RESULT', returnedData);
+        // return returnedData
 
         // PythonShell.run('adafruit_python_bpm.py', options, function (err, results) {
         //   if (err) throw err;
@@ -35,5 +53,57 @@ module.exports = NodeHelper.create({
         //   console.log('adafruit_python_bpm.py results: %j', results[0]);
         //   self.sendSocketNotification('BMP_DATA_RESULT', results[0]);
         // });
+    },
+
+    saveNewBmpEntryInDatabase: function () {
+        /**
+         * This method will insert the current pressure and insert it into the database with the current timestamp
+         */
+
+        let bmpData = this.getBmpData();
+        let dateNow = new Date();
+
+        let datedBmp = {
+            date: dateNow,
+            bmpData: bmpData
+        };
+
+        this.db.insert(datedBmp, function (err, newDoc) { // Callback is optional
+            console.log("New data saved");
+        });
+
+        // clean old data
+        this.cleanOutdatedBmpData();
+    },
+
+    cleanOutdatedBmpData: function () {
+        /**
+         * Clean old data from the database
+         * @type {Date}
+         */
+        let limitDateBmpData = new Date();
+        console.log(limitDateBmpData);
+        console.log(this.helperConfig.limitKeepBmpData);
+        limitDateBmpData.setMilliseconds(limitDateBmpData.getMilliseconds() - this.helperConfig.limitKeepBmpData);
+        console.log(limitDateBmpData);
+        let self = this;
+        this.db.find({}, function (err, bmpData) {
+            for (let i = 0; i < bmpData.length; i++) {
+                console.log(bmpData[i]);
+                if (bmpData[i].date <  limitDateBmpData ){
+                    self.db.remove(bmpData[i], {}, function (err, numRemoved){
+                        console.log(bmpData[i].date + "removed");
+                    });
+                }
+            }
+        });
+    },
+    
+    getCurrentDatabase: function () {
+        let self = this;
+        this.db.find({}, function (err, bmpData) {
+            self.sendSocketNotification('DATABASE_RESULT', bmpData);
+        });
     }
+
 });
